627003d63d039ebdac6c538b57704213d1c99fb7
jiyun921 <ryn0921@naver.com>
2026-02-06 15:22:58 +0900

    [chore] Spring Boot 프로젝트 초기 생성

---
+++ src/main/java/com/konkuk/propit/PropitApplication.java
@@ -0,0 +1,13 @@
+package com.konkuk.propit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropitApplication.class, args);
+	}
+
+}
