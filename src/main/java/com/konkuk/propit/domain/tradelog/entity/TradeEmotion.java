b28dde52f4ab201ffbe4d51cb68c1531d0de7680
simchaehyeon <tlacogussla@konkuk.ac.kr>
2026-02-22 17:20:16 +0900

    [FEAT] TradeEmotion 엔티티 추가

--- src/main/java/com/konkuk/propit/domain/tradelog/entity/TradeEmotion.java
+++ src/main/java/com/konkuk/propit/domain/tradelog/entity/TradeEmotion.java
@@ -1,4 +1,25 @@
 package com.konkuk.propit.domain.tradelog.entity;
 
+import com.konkuk.propit.domain.emotion.entity.Emotion;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
 public class TradeEmotion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "trade_log_id")
+    private TradeLog tradeLog;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "emotion_id")
+    private Emotion emotion;
 }
