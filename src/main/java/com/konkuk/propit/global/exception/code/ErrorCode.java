0bd6c76f3c6c3e96acfa7a507a91c450edd368c0
jiyun921 <ryn0921@naver.com>
2026-03-22 20:22:45 +0900

    [refactor] 공통 응답/에러 구조 수정

---
+++ src/main/java/com/konkuk/propit/global/exception/code/ErrorCode.java
@@ -0,0 +1,33 @@
+package com.konkuk.propit.global.exception.code;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum ErrorCode {
+
+    // COMMON
+    INVALID_INPUT(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 오류입니다."),
+
+    // JWT
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다."),
+    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 토큰입니다."),
+    REFRESH_TOKEN_NOT_FOUND(HttpStatus.UNAUTHORIZED, "Refresh Token이 존재하지 않습니다."),
+    REFRESH_TOKEN_MISMATCH(HttpStatus.UNAUTHORIZED, "Refresh Token이 일치하지 않습니다."),
+
+
+    // User
+    EMAIL_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 존재하는 이메일입니다."),
+    NICKNAME_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 존재하는 닉네임입니다."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
+    ;
+
+    private final HttpStatus status;
+    private final String message;
+
+    ErrorCode(HttpStatus status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+}
---
+++ src/main/java/com/konkuk/propit/global/response/SuccessCode.java
@@ -0,0 +1,20 @@
+package com.konkuk.propit.global.response;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum SuccessCode {
+
+    USER_SIGNUP_SUCCESS(HttpStatus.OK.value(), "회원가입에 성공했습니다."),
+    USER_LOGIN_SUCCESS(HttpStatus.OK.value(), "로그인에 성공했습니다.");
+
+    private final int code;
+    private final String message;
+
+    SuccessCode(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+}
