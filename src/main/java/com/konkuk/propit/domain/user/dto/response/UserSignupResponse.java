9cdd133b0beb8aee57640da0cbb09a8a6506bb47
jiyun921 <ryn0921@naver.com>
2026-03-23 16:22:09 +0900

    [fix] 회원가입 응답 필드 수정

--- src/main/java/com/konkuk/propit/domain/user/dto/response/UserSignupResponse.java
+++ src/main/java/com/konkuk/propit/domain/user/dto/response/UserSignupResponse.java
@@ -3,15 +3,11 @@ package com.konkuk.propit.domain.user.dto.response;
 import com.konkuk.propit.domain.user.entity.User;
 
 public record UserSignupResponse(
-        Long id,
-        String email,
-        String nickname
+        Long id
 ) {
     public static UserSignupResponse from(User user) {
         return new UserSignupResponse(
-                user.getId(),
-                user.getEmail(),
-                user.getNickname()
+                user.getId()
         );
     }
 }
