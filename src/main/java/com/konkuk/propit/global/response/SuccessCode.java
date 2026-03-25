51a2e25f77d0ced8f3b2bb036ef9b9afe43d88eb
simchaehyeon <tlacogussla@konkuk.ac.kr>
2026-03-25 19:41:01 +0900

    [FEAT] tradelog 상세조회 성공 코드 추가

--- src/main/java/com/konkuk/propit/global/response/SuccessCode.java
+++ src/main/java/com/konkuk/propit/global/response/SuccessCode.java
@@ -8,8 +8,11 @@ public enum SuccessCode {
 
     TRADELOG_CREATE_SUCCESS(HttpStatus.OK.value(), "매매 기록 생성에 성공했습니다."),
     TRADELOG_UPDATE_SUCCESS(HttpStatus.OK.value(), "매매 기록 수정에 성공했습니다."),
+    TRADELOG_DETAIL_SUCCESS(HttpStatus.OK.value(), "매매 기록 조회에 성공했습니다."),
+
     USER_SIGNUP_SUCCESS(HttpStatus.CREATED.value(), "회원가입에 성공했습니다."),
     USER_LOGIN_SUCCESS(HttpStatus.OK.value(), "로그인에 성공했습니다.");
+
 
     private final int code;
     private final String message;
